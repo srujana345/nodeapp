c3cd57145bf8f54be3e37492c98376b714cbf6b5
srujana345 <sai.srujana144@gmail.com>
2025-05-01 15:09:16 +0530

    new

--- index.js
+++ index.js
@@ -1,46 +1,34 @@
-// console.log('Helooww')
-
-// let a=20
-// let b=30
-// let c=a+b
-// console.log(c)
-
-// function sqr(x){
-//     return x*x
-// }
-// console.log(sqr(5))
-
-// const sq=(a)=>{
-//     return a*a
-// }
-// console.log(sq(5))
-
-// import cors from 'cors'
-import express from 'express'
-const app=express();
-// app.use(cors())
-app.listen(8081,()=>{
-    console.log('Server is running on port 8081')
+import express from "express";
+import cors from "cors";
+import { MongoClient ,ObjectId} from "mongodb";
+const app = express();
+app.use(cors());
+app.use(express.json());
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+const db = client.db("mernappdb");
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
-app.get("/",(req,res)=>{
-    res.send("Hello World")
-})
-// app.get("/home",(req,res)=>{
-//     res.send("Home page")
-// })
-// app.get("/contact",(req,res)=>{
-//     res.send("Contact Page")
-// })
-// app.get("/faq",(req,res)=>{
-//     res.send("FAQ Page")
-// })
+app.get("/products", async (req, res) => {
+  const products = await db.collection("products").find().toArray();
+  res.json(products);
+});
 
-app.get('/products',(req,res)=>{
-    const products=[
-        {id:1,name:'Product 1',price:100},
-        {id:2,name:'Product 2',price:200},
-        {id:3,name:'Product 3',price:300},
-        
-    ];
-    res.json(products)
+app.post("/products", async (req, res) => {
+  const { name, price } = req.body;
+  const newProduct = {
+    name: name,
+    price: price,
+  };
+  const products = await db.collection("products").insertOne(newProduct);
+  res.json(products);
+});
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const products = await db.collection("products").deleteOne({ _id: new ObjectId(id) });
+  res.json(products);
+});
+app.listen(8081, () => {
+  console.log("Server started on port 8081");
 });
